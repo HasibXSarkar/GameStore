caac22bc4547b2042eb56ea1201f3822294554b7
HasibXSarkar <sarkar.hasib212@gmail.com>
2025-10-25 23:34:59 +0600

    program.cs improved

--- GameStore.api/Program.cs
+++ GameStore.api/Program.cs
@@ -1,6 +1,36 @@
+using GameStore.api.Dtos;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+List<GameDto> games = [
+	new(
+		1,
+		"Street Fighter II",
+		"Fighting",
+		19.99M,
+		new DateOnly(1992, 7, 15)),
+	new(
+		2,
+		"Mortal Kombat",
+		"Fighting",
+		29.99M,
+		new DateOnly(1992, 10, 8)),
+	new(
+		3,
+		"The Legend of Zelda: A Link to the Past",
+		"Action-Adventure",
+		39.99M,
+		new DateOnly(1991, 11, 21),
+
+
+	)
+
+
+
+
+
+	)
+]
 app.MapGet("/", () => "Hello From C# API");
 
 app.Run();
