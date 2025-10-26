4f809ad72c575249dcd9f2b3669b42fdad23a862
HasibXSarkar <sarkar.hasib212@gmail.com>
2025-10-26 19:36:09 +0600

    put endpoint done

--- GameStore.api/obj/Debug/net8.0/GameStore.api.AssemblyInfo.cs
+++ GameStore.api/obj/Debug/net8.0/GameStore.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9633c77e2c9df73fdf4b54cbae6cfa930ac0c9fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
