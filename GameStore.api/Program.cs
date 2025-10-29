e042baeeca98b552b2fb21f1a18293c5ea52c804
HasibXSarkar <sarkar.hasib212@gmail.com>
2025-10-29 21:51:42 +0600

    database migration created

--- GameStore.api/Program.cs
+++ GameStore.api/Program.cs
@@ -1,5 +1,13 @@
 using GameStore.api.Endpoints;
+using GameStore.api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connstring = builder.Configuration.GetConnectionString("GameStore");
+builder.Services.AddSqlite<GameStoreContext>(connstring);
+
 var app = builder.Build();
+
 app.MapGamesEndpoints();
+
 app.Run();
